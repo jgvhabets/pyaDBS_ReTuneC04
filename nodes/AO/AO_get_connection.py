33a0b7e520767c2f05c67844393ab8498a8fc15e
Jeroen Habets <jgvhabets@gmail.com>
2023-12-14 17:40:19 +0100

    WIP: started work on AO nodes with neuro-omega matlab wrapper

---
+++ nodes/AO/AO_get_connection.py
@@ -0,0 +1,64 @@
+"""
+establish connection with AlphaOmega's
+NeuroOmega
+"""
+import time
+import neuroomega_matlab as no
+
+
+def connect_AO(AO_connection: str = 'MATLAB',
+               AO_MAC: str = "F4:5E:AB:6B:6D:A1"):
+    
+    assert AO_connection.upper() in ['MATLAB', 'C'], 'invalid AO_connection'
+    
+    if AO_connection.upper() == 'MATLAB':
+        # use AO matlab wrapper
+        no_engine = no.get_engine()
+
+        if no_engine.AO_IsConnected():
+            no_engine.AO_CloseConnection()
+        
+        no_engine.AO_DefaultStartConnection(AO_MAC)
+        
+        for n in range(10):  # give 10 seconds for AO connection
+            time.sleep(1)
+            
+            if no_engine.AO_IsConnected():  # no.isConnected():
+                print("Connection to NeuroOmega established.")
+                
+                return no_engine    
+        
+        if not no_engine.AO_IsConnected():  # no.isConnected():
+            raise ValueError(
+                "Connection to NeuroOmega could not be established."
+                "Ending program. Please check the connection and try again."
+            )
+    
+
+    elif AO_connection.upper() == 'C':
+
+        print('to make')
+        raise ValueError('not existing AO-C++ connector')
+
+
+def apply_and_stop_test_stim(no_engine):
+
+    try:
+        # apply short test stim
+        no_engine.AO_DefaultStimulation(
+            StimFreqRight=130,
+            StimAmpRight_mA=0.5,
+            StimFreqLeft=23,
+            StimAmpLeft_mA=0.5,
+            Duration_Sec=10.0,
+        )
+        print('test stim succesfully started')
+
+        for i in range(5): time.sleep(5)  # wait for 5 seconds
+
+        no_engine.AO_DefaultStopStimulation()
+        print('test stim succesfully started')
+
+    except:
+        print('Error caused in test stim, NO being closed...')
+        closed = no_engine.AO_CloseConnection()
---
+++ nodes/AO/AO_stim_maltab.py
@@ -0,0 +1,79 @@
+"""
+Perform DBS via AlphaOmega NeurOmega
+
+to test run alone (WIN): python -m nodes.AO.AO_stim_matlab
+
+chronic stim source: https://github.com/jlbusch/C04/blob/dev/stim_AO/stim_AO.m
+"""
+# import public packages
+import time
+from pandas import DataFrame
+from timeflux.core.node import Node
+
+# import custom neuroomega matlab wrapper (credits: Richard Koehler) (located in REPO/packages/neuroomega_matlab)
+import neuroomega_matlab as no
+
+# import repo functions
+from .AO_get_connection import (
+    connect_AO,
+    apply_and_stop_test_stim
+)
+
+class AO_stim(Node):
+    """
+    
+    Raises:
+        - ValueError if incorrect input_signal is given
+    
+    """
+    def __init__(
+        self, macNO = 'F4:5E:AB:6B:6D:A1',
+        AO_connection: str = 'matlab',
+    ):
+        # connect to AO    
+        self.no_engine = connect_AO(AO_connection=AO_connection,
+                                    AO_MAC=macNO)
+
+        # test stim start and stop
+        try:
+            apply_and_stop_test_stim(self.no_engine)
+
+        except:
+            closed = self.no_engine.AO_CloseConnection()
+        
+    
+    def update(self):
+        # is executed every time its activated by timeflux graph
+        input_value =  self.i.data.values[0, 0]
+        # print('input value:', input_value, int(input_value > self._threshold))
+        output = int(input_value > self._threshold)
+        # print('EXC THRESH', output)
+
+        # TODO: aDBS decision making based on input signal
+        # if stim should be switched on
+        if True:
+            self.no_engine.AO_DefaultStimulation(
+                StimFreqRight=130,
+                StimAmpRight_mA=0.5,
+                StimFreqLeft=23,
+                StimAmpLeft_mA=0.5,
+                Duration_Sec=10.0,
+            )
+        
+        # if stim should be switched off
+        if True:
+            self.no_engine.AO_DefaultStopStimulation()
+
+        # sets as pandas DataFrame
+        self.o.data = DataFrame(
+            data=[[input_value, output]],
+            columns=['IN (biomarker)',
+                     'OUT (aDBS trigger)'],
+            index=self.i.data.index
+        )
+        # print(self.o.data)
+    
+
+    def close(self):
+        closed = self.no_engine.AO_CloseConnection()
+        print('closed NeuroOmega connection')
